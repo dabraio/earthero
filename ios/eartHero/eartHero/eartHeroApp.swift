de150c75e4e3fb67dd4bc2cecb1588f9a2d781a7
Faustino Dabraio da Silva <16191058+dabraio@users.noreply.github.com>
2021-08-28 19:57:27 +0100

    Initial project setup
    Initial project setup

---
+++ ios/eartHero/eartHero/eartHeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  eartHeroApp.swift
+//  eartHero
+//
+//  Created by Faustino Dabraio da Silva on 28/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct eartHeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
